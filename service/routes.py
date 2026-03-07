93190255ab04ff31099f3b71d0c12bd4b1811993
NUMBNUT68 <tristankrauss123@gmail.com>
2026-03-07 13:41:20 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,8 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """Returns all of the Accounts"""
@@ -76,6 +78,7 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -100,6 +103,8 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -126,6 +131,8 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
